81bf27ae9f7590a6b166c20291fd2baa404f465b
xenyc1337 <aleqsandredoxnadze@gmail.com>
2025-01-15 22:30:51 +0400

    fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -16,6 +17,10 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   title: "xenycx",
   description: "ლინკების ჯოჯოხეთი",
+  keywords: ["portfolio", "developer", "projects"],
+  viewport: "width=device-width, initial-scale=1",
+  robots: "index, follow",
+  themeColor: "#ADD8E6"
 };
 
 export default function RootLayout({
@@ -24,7 +29,28 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ge">
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta property="og:site_name" content="ლინკების ჯოჯოხეთი" />
+        <meta property="og:image" content="https://avatars3.githubusercontent.com/u/50420166?s=300" />
+        <meta property="og:description" content="View Alesandre Dokhnadze’s Linktree." />
+        <meta property="og:title" content="Aleksandre Dokhnadze | xenyc | Linktree" />
+        <meta name="description" content="!" />
+        <meta name="author" content="xenyc" />
+        <link rel="icon" href="/favicon.ico" />
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-F1MLQC4TK3" />
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-F1MLQC4TK3" />
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-F1MLQC4TK3');
+          `}
+        </script>
+        <script defer src="https://cloud.umami.is/script.js" data-website-id="a37e2a42-77e7-4095-9798-364931be22ea"></script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
